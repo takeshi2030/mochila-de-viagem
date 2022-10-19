bd545c181be27d4bdf4abc630c14afa5a2b72b70
André Maemura <andremaemura@gmail.com>
2022-10-19 12:39:52 -0300

    finalizacao projeto

--- js/main.js
+++ js/main.js
@@ -12,14 +12,26 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+   
     const itemAtual = {
         'nome': nome.value,
         'quantidade': quantidade.value
     }
 
-    criaElemento(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
+
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1]).id + 1 : 0
+
+        criaElemento(itemAtual)
     
-    itens.push(itemAtual)
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem('itens', JSON.stringify(itens))
 
@@ -34,9 +46,35 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome // += significa receber
 
+    novoItem.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(novoItem)    
+}
+
+function atualizaElemento(item) {
+    document.querySelector('[data-id="'+item.id+'"]').innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement('button')
+    elementoBotao.innerHTML = 'X'
+
+    elementoBotao.addEventListener('click', function() {
+        deletaElemento(this.parentNode, id)
+    })
+    
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem('itens', JSON.stringify(itens))
 }
